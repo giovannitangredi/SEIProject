0294e02c00b6b295767445c8d8ec35b070c0c1c5
ENRICO CLEMENTE s267559 <s267559@studenti.polito.it>
2020-05-08 11:07:16 +0200

    docs: added repository package and implemented interfaces

---
+++ src/main/java/it/polito/ezgas/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package it.polito.ezgas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.polito.ezgas.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
