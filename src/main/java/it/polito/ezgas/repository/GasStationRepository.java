8a905d19c338d6c18aef5e13c0897a1fa05f0437
giovannitangredi <62295204+giovannitangredi@users.noreply.github.com>
2020-05-08 15:44:09 +0200

    fix: minor fix in GasStationRepository

--- src/main/java/it/polito/ezgas/repository/GasStationRepository.java
+++ src/main/java/it/polito/ezgas/repository/GasStationRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.polito.ezgas.entity.GasStation;
 
-public interface GasStationRepository extends JpaRepository<GasStation, Long>{
+public interface GasStationRepository extends JpaRepository<GasStation, Integer>{
 
 }
