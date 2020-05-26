b4e9fb4c8faeda9d18cd43c24c7024642b3775f2
giovannitangredi <62295204+giovannitangredi@users.noreply.github.com>
2020-05-26 19:27:55 +0200

    fix: minor fix in BootEZGasApplication.java

--- src/main/java/it/polito/ezgas/BootEZGasApplication.java
+++ src/main/java/it/polito/ezgas/BootEZGasApplication.java
@@ -3,15 +3,21 @@ package it.polito.ezgas;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.polito.ezgas.entity.User;
+import it.polito.ezgas.repository.UserRepository;
+
 @SpringBootApplication
 public class BootEZGasApplication {
-
+	@Autowired
+	UserRepository repo;
 	public static void main(String[] args) {
 		SpringApplication.run(BootEZGasApplication.class, args);
 	}
@@ -22,7 +28,13 @@ public class BootEZGasApplication {
 		Connection conn = DriverManager.getConnection("jdbc:h2:./data/memo", "sa", "password");
 		conn.close();
 		
-		
+		Boolean anyAdmin=repo.findAll().stream().filter((user)->  user.getAdmin()).collect(Collectors.toList()).size()>0;
+		if(!anyAdmin)
+		{
+			User user= new User("admin", "admin", "admin@ezgas.com", 5);
+			user.setAdmin(true);
+			repo.save(user);
+		}
 		/*
 		 
 		list all the users stored in the database and, if there is no an admin user create it
